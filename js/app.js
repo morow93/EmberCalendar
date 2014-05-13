28f3b0d0d87daca9a937742c390610353274d2f6
derchepur <derchepur@gmail.com>
2014-05-13 16:49:58 +0400

    finish with route

--- js/app.js
+++ js/app.js
@@ -1,11 +1,23 @@
 Application = Ember.Application.create({
     ready: function(){
-        //alert('Вы сделали это!');
+        //alert('Проверка...');
     }
 });
 
 Application.Router.map(function () {
-    this.resource('application/index', { path: '/' }, function () {   
-        this.route('datecalendar');
-  });
+    this.route('yearmonthday', { path:
+    "year/:year/month/:month/day/:day"});
+});
+
+Application.IndexRoute = Em.Route.extend({
+    redirect: function (model) {
+        
+        var current = Ember.Object.create({
+            year: new Date().getFullYear(),
+            month: new Date().getMonth() + 1,
+            day: new Date().getDate()
+        });
+
+        this.transitionTo('yearmonthday', current);
+    }
 });
