e2ad96766560b2c5d4b25ceca23c8687667fef4b
morow93 <derchepur@gmail.com>
2014-05-15 00:09:49 +0400

    refs #5 add_calendar_grid

--- js/app.js
+++ js/app.js
@@ -3,22 +3,3 @@
         //alert('Проверка...');
     }
 });
-
-Application.Router.map(function () {
-    this.route('yearmonthday', { path:
-    "year/:year/month/:month/day/:day"
-    });
-});
-
-Application.IndexRoute = Em.Route.extend({
-    redirect: function () {
-        
-        var model = Ember.Object.create({
-            year: new Date().getFullYear(),
-            month: new Date().getMonth() + 1,
-            day: new Date().getDate()
-        });
-
-        this.transitionTo('yearmonthday', model);
-    }
-});
--- js/controllers/YearmonthdayController.js
+++ js/controllers/YearmonthdayController.js
@@ -17,33 +17,57 @@ Application.YearmonthdayController = Ember.ObjectController.extend({
                            ];
         return twelveMonths[(Number)(this.get('month'))];
     }.property('month'),
-    
-    transitionModel: function(){
-            var model = Ember.Object.create({
-                year: this.get('year'),
-                month: this.get('month'),
-                day: this.get('day')
-            });
-            return model;
-    }.property('year','month','day'),
       
     didChange: function() {
-        //console.log('change properties');
+        this.transitionToRoute('yearmonthday',this.get('year'), this.get('month'), this.get('day'));
     }.observes('year', 'month', 'day'),
+   
+   getMonth: function() {
+          
+        var quantityOfDayInEachMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+        if (this.get('year') % 4 === 0) ++quantityOfDayInEachMonth[1];        
+        
+        var totalDays = quantityOfDayInEachMonth[this.get('month') - 1];
+        var eachDay = 1;
+          
+        var dayOfWeek = new Date(this.get('year'), (Number)(this.get('month')) - 1, 1).getDay();
+        dayOfWeek = dayOfWeek === 0 ? 7 : dayOfWeek;
+
+        var mas = [];
+        mas[0] = [];
+        for (var k = 0; k < 7; ++k){
+            if (k >= (dayOfWeek - 1)){
+                mas[0][k] = eachDay++;
+                
+            }
+            else{
+                mas[0][k]="*";
+            }
+        }    
+        var quantityOfArrays = Math.ceil((totalDays - eachDay)/7);
+        for(var i = 1; i < quantityOfArrays + 1; ++i){
+            mas[i] = [];
+            for(var j = 0; j < 7; ++j){
+                if (eachDay <= totalDays){
+                    mas[i][j] = eachDay++;
+                }
+                else
+                    mas[i][j] = "*";
+            }
+        }
+          
+        return mas;
+      }.property('month', 'year'),
                        
     actions: {
           
       prevYear: function () {
         if (this.get('year') > 0)
             this.set('year', (Number)(this.get('year')) - 1);
-    
-        this.transitionToRoute('yearmonthday', this.get('transitionModel'));
       },
       
       nextYear: function () {
         this.set('year', (Number)(this.get('year')) + 1);
-          
-        this.transitionToRoute('yearmonthday', this.get('transitionModel'));
       },
           
       nextMonth: function() {
@@ -53,18 +77,17 @@ Application.YearmonthdayController = Ember.ObjectController.extend({
         }
         else{
             this.set('month', (Number)(this.get('month')) + 1);        
-            this.transitionToRoute('yearmonthday', this.get('transitionModel'));
         }
       },
+          
       prevMonth: function() {
         if (this.get('month') <= 1){
             this.set('month', 12);
             this.send('prevYear');
         }
         else{
-            this.set('month', (Number)(this.get('month')) - 1);        
-            this.transitionToRoute('yearmonthday', this.get('transitionModel'));
+            this.set('month', (Number)(this.get('month')) - 1);
         }
-      }
+      },
     }
 });
